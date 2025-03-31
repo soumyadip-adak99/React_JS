febe8029d3240a30b782830492f96876bc419dd7
Soumyadip Adak <149286607+adak99@users.noreply.github.com>
2025-03-31 12:04:33 +0530

    update

--- My-profolio-website/src/App.jsx
+++ My-profolio-website/src/App.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./pages/home/Home.jsx";
 import About from "./pages/about/About.jsx";
 import Portfolio from "./pages/portfolio/Portfolio.jsx";
 import Contact from "./pages/contact/Contact.jsx";
 import AppLayout from "./component/AppLayout.jsx";
-import ScrollToTop from "./component/ScrollToTop.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
+
+    return null;
+};
 
 const App = () => {
     const router = createBrowserRouter([
         {
             path: '/',
-            element: <AppLayout />,
+            element: (
+                <AppLayout>
+                    <ScrollToTop />
+                </AppLayout>
+            ),
             children: [
                 {
-                    path: '/',
+                    index: true,
                     element: <Home />,
                 },
                 {
@@ -32,7 +45,6 @@ const App = () => {
             ],
         },
     ]);
-
 
     return <RouterProvider router={router} />;
 };
