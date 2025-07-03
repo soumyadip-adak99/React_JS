ef81d6023f09e99fb744b6146a21d3149cc5a6c3
soumya <work.soumyadipadak@gmail.com>
2025-07-04 01:14:25 +0530

    update

--- Intership project front end/src/router/RootRoute.jsx
+++ Intership project front end/src/router/RootRoute.jsx
@@ -2,15 +2,14 @@ import Home from '../pages/home/Home';
 import Admin from '../pages/admin/Admin';
 import LandingPage from '../pages/LandingPage/LandingPage';
 import { useAuth } from "../context/AuthContext";
+import LoadingScaliton from '../constants/LoadingScaliton';
 
 export default function RootRoute() {
     const { isAuthenticated, user, loading } = useAuth();
 
     if (loading) {
         return (
-            <div className="h-screen flex items-center justify-center">
-                <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-            </div>
+            <LoadingScaliton />
         );
     }
 
