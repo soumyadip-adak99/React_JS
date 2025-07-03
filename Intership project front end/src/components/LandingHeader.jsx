19a696aef1130f48fbab518c6656d446949f9458
soumya <work.soumyadipadak@gmail.com>
2025-07-04 00:22:36 +0530

    update

--- Intership project front end/src/components/LandingHeader.jsx
+++ Intership project front end/src/components/LandingHeader.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function LandingHeader() {
     return (
         <>
-            <div className="fixed top-0 left-0 right-0 py-4 bg-black/30 backdrop-blur-sm z-50">
+            <div className="fixed top-0 left-0 right-0 py-4 backdrop-blur-sm z-50">
                 <div className="container mx-auto px-4">
                     <div className="flex justify-center md:justify-start">
                         <Link to={"/"}>
