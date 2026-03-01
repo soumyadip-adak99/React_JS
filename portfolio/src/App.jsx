94219f001dc80558c754c27c0be9a1f08c45cae9
unknown <soumya.dip.adak@outlook.com>
2026-03-01 21:48:33 +0530

    remove feature to not show inspect

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,21 +5,21 @@ import Index from "./pages/Index";
 import Scrolltop from "./utils/Scrolltop";
 
 const App = () => {
-    useEffect(() => {
-        document.addEventListener("contextmenu", (e) => e.preventDefault());
+    // useEffect(() => {
+    //     document.addEventListener("contextmenu", (e) => e.preventDefault());
 
-        document.addEventListener("keydown", (e) => {
-            if (
-                e.key === "F12" ||
-                (e.ctrlKey && e.shiftKey && e.key === "I") ||
-                (e.ctrlKey && e.shiftKey && e.key === "C") ||
-                (e.ctrlKey && e.shiftKey && e.key === "J") ||
-                (e.ctrlKey && e.key === "U")
-            ) {
-                e.preventDefault();
-            }
-        });
-    }, []);
+    //     document.addEventListener("keydown", (e) => {
+    //         if (
+    //             e.key === "F12" ||
+    //             (e.ctrlKey && e.shiftKey && e.key === "I") ||
+    //             (e.ctrlKey && e.shiftKey && e.key === "C") ||
+    //             (e.ctrlKey && e.shiftKey && e.key === "J") ||
+    //             (e.ctrlKey && e.key === "U")
+    //         ) {
+    //             e.preventDefault();
+    //         }
+    //     });
+    // }, []);
 
     return (
         <>
